19c72d7338bbbc27d07ba3a2103e3027df9fa6b5
DiyRex <75021968+DiyRex@users.noreply.github.com>
2024-04-02 10:23:07 +0530

    htaccess added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $uri =  $_SERVER['REQUEST_URI'];
 $uriParts = parse_url($uri);
 $Route = $uriParts['path'];
 
+
 if (array_key_exists($Route, $routes)) {
     // Include the file corresponding to the route
     include $routes[$Route];
