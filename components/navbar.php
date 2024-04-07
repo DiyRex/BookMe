1c10ac5ae767e5bc0356d614679dcc7e1ed8fdf4
DiyRex <75021968+DiyRex@users.noreply.github.com>
2024-04-07 12:40:59 +0530

    navbar changed

--- components/navbar.php
+++ components/navbar.php
@@ -2,9 +2,12 @@
 @session_start();
 echo '
 <!-- Navbar -->   
+    <head>
+    <link rel="stylesheet" href="../components/navbar.css">
+    </head>
     <nav class="navbar navbar-expand-lg navobj">
       <div class="col-3 col-md-5">
-        <a class="navbar-brand text-white" href="/">BoardMe</a>
+        <a class="navbar-brand text-white" href="/">BookMe</a>
       </div>
       <button
         class="togbtn navbar-toggler"
@@ -57,7 +60,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'Student' && $_SESSION['lo
 }
 echo '
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Contact US</a>
+            <a class="nav-link text-white" href="#">Contact Us</a>
           </li>
         </ul>
         
